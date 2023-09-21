4a83560242385ff2ffd44c08041732ac4e0a064a
Brook Frye <bfrye@council.nyc.gov>
2023-09-21 16:47:58 -0400

    add codes f first it of this

---
+++ code/soa_tax_bills_scrape.R
@@ -0,0 +1,33 @@
+# are the current rs data reliable -- what if a bbl was on the 2017 but show's all nas for future years
+# which(na_bbls$V1 %in% 1005690020)
+
+source("code/utils/00_load_dependencies.R")
+
+soa_scrape_fnx <- function(bbls) {
+
+soa_list <- list()
+
+  for(i in 1:nrow(bbls)){
+    bbl <- na_bbls2[i]
+    soa_list[i] <- paste0('https://a836-edms.nyc.gov/dctm-rest/repositories/dofedmspts/StatementSearch?bbl=', bbl, '&stmtDate=20210604&stmtType=SOA')
+  }
+  
+# re-org text 
+soa_ocr_l <- list()
+soa_ocr_l2 <- list()
+
+for (i in 1:nrow(na_bbls)) {
+  
+  soa_ocr_l[i] <- pdf_text(soa_list[[i]])
+  
+}
+
+soa_ocr2 <- lapply(soa_ocr_l, function(x) str_split(x, "\n"))
+
+return(soa_ocr2)
+
+}
+as.data.table(unlist(lapply(soa_ocr2, function(x)grep("Rent Stabilization", x))))
+
+
+
